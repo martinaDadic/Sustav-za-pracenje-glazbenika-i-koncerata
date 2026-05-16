5be4fb2656a35d0c40e2398359ee2aefdd50afda
Lucija Lasović <llasovic@Lucijas-MacBook-Pro.local>
2026-05-16 10:15:35 +0200

    setup backend

---
+++ spgk-backend/src/test/java/hr/fer/spgk_backend/SpgkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.spgk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
